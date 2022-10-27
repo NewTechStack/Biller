5065cd9f1ab75cacffaf0e82d4aacd79f2fd4f58
SCcagg5 <sccagg5@gmail.com>
2022-10-27 15:37:36 +0200

    Update timesheet.py

--- back/src/Model/timesheet.py
+++ back/src/Model/timesheet.py
@@ -8,9 +8,9 @@ def timesheet_get_all(cn, nextc):
     match = {'field': 'id', "value": "*/" + cn.rt["folder"] + "/" } if 'folder' in cn.rt else match
     match = {'field': 'id', "value": cn.rt["client"]  + "/" + cn.rt["folder"] + "/" } if 'folder' in cn.rt and "client" in cn.rt else match
     greater = None if "greater" not in cn.pr else greater
-    greater = None if "field" not in greater or "value" not in greater else greater 
+    greater = None if greater is None or "field" not in greater or "value" not in greater else greater 
     less = None if "less" not in cn.pr else less
-    less = None if "field" not in less or "value" not in less else less 
+    less = None if less is None or "field" not in less or "value" not in less else less 
     err = check.contain(cn.pr, ["filter", "exclude"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
