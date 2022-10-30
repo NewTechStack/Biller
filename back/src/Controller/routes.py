36ac8610ae3e4626b8a62471605de3d07ba21af1
SCcagg5 <sccagg5@gmail.com>
2022-10-31 00:33:57 +0100

    Update routes.py

--- back/src/Controller/routes.py
+++ back/src/Controller/routes.py
@@ -29,18 +29,19 @@ def setuproute(app, call):
     @app.route('/client/<>/folder/<>',                  ['OPTIONS', 'PUT'],           lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, folder_edit])       )
     @app.route('/client/<>/folder/<>',                  ['OPTIONS', 'DELETE'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, folder_delete])     )
 
-    @app.route('/timesheets',                            ['OPTIONS', 'POST'],          lambda x = None: call([sso_verify_token, timesheet_get_all])                      )
+    @app.route('/timesheets',                           ['OPTIONS', 'POST'],          lambda x = None: call([sso_verify_token, timesheet_get_all])                      )
     @app.route('/user/<>/timesheets',                   ['OPTIONS', 'POST'],          lambda x = None: call([sso_verify_token, user_set_by_id, user_exist, timesheet_get_all])                      )
     @app.route('/client/<>/timesheets',                 ['OPTIONS', 'POST'],          lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, timesheet_get_all])                      )
     @app.route('/client/<>/folder/<>/timesheet',        ['OPTIONS', 'POST'],          lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_new, timesheet_edit])                      )
-    @app.route('/client/<>/folder/<>/timesheet/<>',      ['OPTIONS', 'GET'],           lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_get])                      )
-    @app.route('/client/<>/folder/<>/timesheet/<>',      ['OPTIONS', 'PUT'],           lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_edit])                      )
-    @app.route('/client/<>/folder/<>/timesheet/<>',      ['OPTIONS', 'DELETE'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_delete])                      )
-
-    @app.route('/client/<>/folder/<>/bills',        ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_get_all])                      )
-    @app.route('/client/<>/folder/<>/bill',         ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_new, bill_edit])                      )
-    @app.route('/client/<>/folder/<>/bill/<>',         ['OPTIONS', 'GET'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_set_by_id, bill_exist, bill_get])                      )
-    @app.route('/client/<>/folder/<>/bill/<>',         ['OPTIONS', 'DELETE'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_set_by_id, bill_exist, bill_delete])                      )
+    @app.route('/client/<>/folder/<>/timesheet/<>',     ['OPTIONS', 'GET'],           lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_get])                      )
+    @app.route('/client/<>/folder/<>/timesheet/<>',     ['OPTIONS', 'PUT'],           lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_edit])                      )
+    @app.route('/client/<>/folder/<>/timesheet/<>',     ['OPTIONS', 'DELETE'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, timesheet_set_by_id, timesheet_exist, timesheet_delete])                      )
     
+    @app.route('/bills',                                ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, bill_get_all])                      )
+    @app.route('/client/<>/bills',                      ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, bill_get_all])                      )
+    @app.route('/client/<>/folder/<>/bills',            ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_get_all])                      )
+    @app.route('/client/<>/folder/<>/bill',             ['OPTIONS', 'POST'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_new, bill_edit])                      )
+    @app.route('/client/<>/folder/<>/bill/<>',          ['OPTIONS', 'GET'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_set_by_id, bill_exist, bill_get])                      )
+    @app.route('/client/<>/folder/<>/bill/<>',          ['OPTIONS', 'DELETE'],        lambda x = None: call([sso_verify_token, client_set_by_id, client_exist, folder_set_by_id, folder_exist, bill_set_by_id, bill_exist, bill_delete])                      )
     def base():
         return
