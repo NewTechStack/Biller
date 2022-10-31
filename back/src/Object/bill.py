f52af7f520d95b17684e6dbb42eeead942c9dc0f
SCcagg5 <sccagg5@gmail.com>
2022-10-31 21:33:45 +0100

    Update bill.py

--- back/src/Object/bill.py
+++ back/src/Object/bill.py
@@ -30,76 +30,82 @@ class Bill(Crud):
           return [False, "Invalid 'TVA_inc' bool", 400]
         data["TVA_inc"] = bool(data["TVA_inc"])
         if data["type"] == "invoice":
-            if not "timesheet" in data or not isinstance(data["timesheet"], list) or not all([isinstance(x, str) for x in data['timesheet']]):
-              return [False, "Invalid 'timesheet' list", 400]
-            timesheets = data["timesheet"]
-            if len(timesheets) == 0:
-                return [False, "Invalid 'timsheet' list", 400]
-            if len(timesheets) != len(set(timesheets)):
-                return [False, "Duplicates in 'timesheet' list", 400]
-            base_id = self.id.rsplit('/', 1)[0]
-            data["price"] = {
-                "HT": 0.0,
-                "taxes": 0.0,
-                "total": 0.0
-            }
-            data["price"]["HT"] = 0.00
-            lines = []
-            for t_id in timesheets:
-                t_id = f"{base_id}/{t_id}"
-                d = Timesheet(t_id).get()
-                if d[1] is None:
-                    return [False, f"Invalid timesheet id: '{t_id}'", 404]
-                if "price" not in d[1] or not any([isinstance(d[1]["price"], x) for x in [int, float]]):
-                    return [False, f"Invalid price in timesheet: '{t_id}'", 400]
-                price = float(d[1]["price"])
-                price_HT =  price
-                if data["TVA_inc"]:
-                    price_HT = price / (1+(tva/100))
-                taxes = price_HT * tva / 100
-                lines.append({
-                    "timesheet_id": t_id,
-                    "price_HT": round(price_HT, 2),
-                    "taxes": round(taxes, 2),
-                    "TVA": tva,
-                    "price": round(price_HT + taxes, 2)
-                })
-                data["price"]["HT"] += price_HT
-            if "fees" in data and isinstance(data["fees"], float) and data["fees"] > 0.0:
-                price_ht = data["price"]["HT"] * data["fees"] / 100
-                data["fees"] = {
-                    "fees": data["fees"],
-                    "price_HT": round(price_ht, 2),
-                    "taxes": round(data["price"]["HT"] * data["fees"] * tva / 10000, 2),
-                    "TVA": tva,
-                    "price": round(data["price"]["HT"] * data["fees"] / 100 + data["price"]["HT"] * data["fees"] * tva / 10000, 2)
-                }
-                data["price"]["HT"] += price_ht
-            if "reduction" in data:
-                if "fix" in data["reduction"]:
-                    if not isinstance(data["reduction"]["fix"], float):
-                        return [False, "Invalid reduction.fix float", 400]
-                    taxes = data["reduction"]["fix"] * tva / 100
-                    data["reduction"]["fix"] = {
-                        "amount": data["reduction"]["fix"],
-                        "value_HT": round(data["reduction"]["fix"], 2)
-                    }
-                    if data["TVA_inc"]:
-                        data["reduction"]["fix"]["value_HT"] = round(data["reduction"]["fix"]["amount"] / (1+(tva/100)), 2)
-                    data["price"]["HT"] -= data["reduction"]["fix"]["value_HT"]
-                if "percentage" in data["reduction"]:
-                    if not isinstance(data["reduction"]["percentage"], float):
-                        return [False, "Invalid reduction.percentage float", 400]
-                    price = data["price"]["HT"] * data["reduction"]["percentage"] / 100
-                    data["reduction"]["percentage"] = {
-                        "amount": data["reduction"]["percentage"],
-                        "value_HT": round(price, 2)
-                    }
-                    data["price"]["HT"] -= price
-            data["price"]["HT"] = round(data["price"]["HT"], 2)
-            data["price"]["taxes"] = round(data["price"]["HT"] * tva / 100, 2)
-            data["price"]["total"] = data["price"]["HT"] + data["price"]["taxes"]
-            data["timesheet"] = lines
+            ret = self.__invoice(data)
+            if ret[0] is False:
+                return ret
+            data = ret[1]
         data["url"] = "/docuement/soon"
         data["status"] = 0
         return self._push(data)
+    
+    def __invoice(self, data):
+        if not "timesheet" in data or not isinstance(data["timesheet"], list) or not all([isinstance(x, str) for x in data['timesheet']]):
+            return [False, "Invalid 'timesheet' list", 400]
+        timesheets = data["timesheet"]
+        if len(timesheets) == 0:
+            return [False, "Invalid 'timsheet' list", 400]
+        if len(timesheets) != len(set(timesheets)):
+            return [False, "Duplicates in 'timesheet' list", 400]
+        base_id = self.id.rsplit('/', 1)[0]
+        data["price"] = {
+            "HT": 0.0,
+            "taxes": 0.0,
+            "total": 0.0
+        }
+        data["price"]["HT"] = 0.00
+        lines = []
+        for t_id in timesheets:
+            t_id = f"{base_id}/{t_id}"
+            d = Timesheet(t_id).get()
+            if d[1] is None:
+                return [False, f"Invalid timesheet id: '{t_id}'", 404]
+            if "price" not in d[1] or not any([isinstance(d[1]["price"], x) for x in [int, float]]):
+                return [False, f"Invalid price in timesheet: '{t_id}'", 400]
+            price_HT =  float(d[1]["price"])
+            if data["TVA_inc"]:
+                price_HT = price_HT / (1+(tva/100))
+            taxes = price_HT * tva / 100
+            lines.append({
+                "timesheet_id": t_id,
+                "price_HT": round(price_HT, 2),
+                "taxes": round(taxes, 2),
+                "TVA": tva,
+                "price": round(price_HT + taxes, 2)
+            })
+            data["price"]["HT"] += price_HT
+        if "fees" in data and isinstance(data["fees"], float) and data["fees"] > 0.0:
+            price_HT = data["price"]["HT"] * data["fees"] / 100
+            data["fees"] = {
+                "fees": data["fees"],
+                "price_HT": round(price_HT, 2),
+                "taxes": round(data["price"]["HT"] * data["fees"] * tva / 10000, 2),
+                "TVA": tva,
+                "price": round(data["price"]["HT"] * data["fees"] / 100 + data["price"]["HT"] * data["fees"] * tva / 10000, 2)
+            }
+            data["price"]["HT"] += price_ht
+        if "reduction" in data:
+            if "fix" in data["reduction"]:
+                if not isinstance(data["reduction"]["fix"], float):
+                    return [False, "Invalid reduction.fix float", 400]
+                taxes = data["reduction"]["fix"] * tva / 100
+                data["reduction"]["fix"] = {
+                    "amount": data["reduction"]["fix"],
+                    "value_HT": round(data["reduction"]["fix"], 2)
+                }
+                if data["TVA_inc"]:
+                    data["reduction"]["fix"]["value_HT"] = round(data["reduction"]["fix"]["amount"] / (1+(tva/100)), 2)
+                data["price"]["HT"] -= data["reduction"]["fix"]["value_HT"]
+            if "percentage" in data["reduction"]:
+                if not isinstance(data["reduction"]["percentage"], float):
+                    return [False, "Invalid reduction.percentage float", 400]
+                price = data["price"]["HT"] * data["reduction"]["percentage"] / 100
+                data["reduction"]["percentage"] = {
+                    "amount": data["reduction"]["percentage"],
+                    "value_HT": round(price, 2)
+                }
+                data["price"]["HT"] -= price
+        data["price"]["HT"] = round(data["price"]["HT"], 2)
+        data["price"]["taxes"] = round(data["price"]["HT"] * tva / 100, 2)
+        data["price"]["total"] = data["price"]["HT"] + data["price"]["taxes"]
+        data["timesheet"] = lines
+        return data
