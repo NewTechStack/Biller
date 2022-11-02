58aa80c98fa4fbf1e61bcc9bcc7978836d84bac7
SCcagg5 <sccagg5@gmail.com>
2022-11-02 02:12:33 +0100

    Update server.py

--- document/src/server.py
+++ document/src/server.py
@@ -1,4 +1,32 @@
 from bottle import route, run
+import requests
+import json
+
+class Pdf():
+    def __init__(self):
+        self.data = None
+
+    def generate(self, html, title):
+        print(html)
+        response = requests.request(
+          "POST",
+          "http://pdfgenerator:8080",
+          headers = {
+                'Content-Type': 'application/json'
+                },
+          data = json.dumps(
+                {
+                    "content": html,
+                    "options":
+                    {
+                        "pageSize": "letter",
+                        "title": title
+                    }
+                }
+            )
+        )
+        return [True, response.content, None]
+
 
 @route('/')
 def index():
