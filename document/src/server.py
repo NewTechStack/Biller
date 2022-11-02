b9a81b47aa8d09007f628dacdadab96aefbf447b
SCcagg5 <sccagg5@gmail.com>
2022-11-02 03:02:58 +0100

    Update server.py

--- document/src/server.py
+++ document/src/server.py
@@ -1,50 +1,11 @@
+import os
 from bottle import route, run
 import requests
 import json
 from jinja2 import Environment, BaseLoader, meta
 
-class Template(Rethink):
-    def __init__(self, name):
-        super().__init__()
-        self.conn = self.conn.table('template')
-        self.name = name
-        self.model = {
-            "name": None,
-            "template": None,
-            "variables": {}
-        }
-        self.data = None
-        self.loaded_template = None
-        self.render = None
 
-    def list(self):
-        ret = list(
-                self.conn.map(
-                    lambda doc: doc['name']
-                ).run()
-              )
-        return [True, ret, None]
-
-    def get(self):
-        if not self.__exist():
-            return [False, "Template doesn't exist", 404]
-        return [True, self.data, None]
-
-    def new(self, template):
-        if self.__exist():
-            return [False, "Template name already exist", 401]
-        variables = meta.find_undeclared_variables(
-                        Environment().parse(
-                            template
-                        )
-                    )
-        data = self.model
-        data['name'] = self.name
-        data['template'] = template
-        data['variables'] = variables
-        self.conn.insert([data]).run()
-        return [True, {'name': self.name}, None]
-
+class Template():
     def customize(self, vars, end = True):
         if not self.__exist():
             return [False, "Template doesn't exist", 404]
@@ -68,13 +29,6 @@ class Template(Rethink):
             return False
         self.loaded_template = Environment(loader=BaseLoader).from_string(self.data['template'])
         return True
-
-    def __exist(self):
-        res = list(self.conn.filter(lambda doc: doc['name'] == self.name).run())
-        if len(res) > 0:
-            self.data = res[0]
-            return True
-        return False
 
 class Pdf():
     def __init__(self):
@@ -105,5 +59,24 @@ class Pdf():
 @route('/')
 def index():
     return "working" 
+
+@route('/templates')
+def index():
+    source = "./source/"
+    template_list = [f for f in os.listdir(source) if not os.path.isfile(os.path.join(source, f))]
+    templates = {}
+    for template in template_list:
+        path = os.path.join(srouce, template, "template.html")
+        if os.path.exists(path) and os.path.isfile(path)
+            f = open(path, "r")
+            templates[template] = {
+                 "variables": meta.find_undeclared_variables(
+                        Environment().parse(
+                            str(f.read())
+                        )
+                    )
+            }
+            f.close()
+    return [tempalte_list, templates]
 
 run(host='0.0.0.0', port=8080)
