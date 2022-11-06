1b61406b4831fb2b82b48b6fdd68b6718f8df2e9
SCcagg5 <sccagg5@gmail.com>
2022-11-06 14:45:49 +0100

    Update bill.py

--- back/src/Object/bill.py
+++ back/src/Object/bill.py
@@ -144,6 +144,7 @@ class Bill(Crud, StatusObject):
         ret = self.get()
         if ret[1] is None:
             retun [False, "error", 500]
+        data = ret[1]
         data["template"]["bucket"] = "files"
         data["template"]["variables"]["num"] = "2022-" + str(int(self.red.filter(
            lambda bill: bill["status"] >= 2
