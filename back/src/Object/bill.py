370fe9d1c1cdf5dbba4a74c335f760af7d7fdbc1
SCcagg5 <sccagg5@gmail.com>
2022-11-08 22:22:38 +0100

    adding

--- back/src/Object/bill.py
+++ back/src/Object/bill.py
@@ -157,6 +157,7 @@ class Bill(Crud, StatusObject):
     def __generate_fact(self, data):
         url = "http://template:8080/template/pdf"
         response = requests.request("POST", url, data=json.dumps(data["template"]), headers={'content-type': "application/json"})
+        print(response.text)
         return json.loads(response.text)
 
     def __currency_format(self, price):
