11182cbc7e84732fd9e3bd22f99d9ecc71ad858d
SCcagg5 <sccagg5@gmail.com>
2022-11-09 01:12:16 +0100

    adding

--- back/src/Model/timesheet.py
+++ back/src/Model/timesheet.py
@@ -16,8 +16,9 @@ def timesheet_get_all(cn, nextc):
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
     if "user_in_charge" in cn.pr["filter"]:
-        cn.pr["filter"]["client_folder"] = [x["id"].split("/")[1] for x in Folder().get_all(1, 10000000, {"user_in_charge": cn.pr["filter"]["user_in_charge"]}, {}, match=None)[1]["list"]]
-        print(cn.pr["filter"]["folder"])
+        cn.pr["filter"]["client_folder"] = []
+        cn.pr["filter"]["client_folder"]["id"] = [x["id"].split("/")[1] for x in Folder().get_all(1, 10000000, {"user_in_charge": cn.pr["filter"]["user_in_charge"]}, {}, match=None)[1]["list"]]
+        print(cn.pr["filter"]["client_folder"])
         del cn.pr["filter"]["user_in_charge"]
     err = Timesheet().get_all(page, number, cn.pr["filter"], cn.pr['exclude'], match=match, greater=greater, less=less)
     return cn.call_next(nextc, err)
