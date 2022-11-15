cca4ad0fb0a80b418e422f40458e7318f365ccdb
SCcagg5 <sccagg5@gmail.com>
2022-11-15 20:44:26 +0100

    Update v2.py

--- back/src/Object/v2.py
+++ back/src/Object/v2.py
@@ -13,7 +13,7 @@ class TimesheetV2():
         page -= 1
         if number < 1:
             number = 1
-        req = self.rt.filter({"client": "193a46bd-10c0-4eec-8390-91b09779ef3f"}).eq_join("client_folder", self.rf).group("right")
+        req = self.rt.filter({"client": "193a46bd-10c0-4eec-8390-91b09779ef3f"}).eqJoin("user", r.db("ged").table("user")).without({"right": "id"}).zip().pluck(["id", "client_folder", "date", "desc", "duration", "price", "first_name", "last_name", "image"]).eqJoin("client_folder", self.rf).group("right").without("right").zip().ungroup().without({"group": {"associate": true, "autrepartie": true, "created_at": true, "created_by": true, "user_in_charge": true, "user_in_charge_price": true}})
         total = int(req.count().run())
         max = math.floor(total / number + 1) if total % number != 0 else int(total/number)
         max = max + 1 if max == 0 else max
