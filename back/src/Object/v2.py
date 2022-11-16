c1960213ecbb0b7595b8b9228b1c88897ede4469
SCcagg5 <sccagg5@gmail.com>
2022-11-16 02:59:58 +0100

    Update v2.py

--- back/src/Object/v2.py
+++ back/src/Object/v2.py
@@ -2,6 +2,42 @@ from .rethink import get_conn, r
 import uuid
 import math
 
+class FolderV2():
+    def __init__(self):
+        self.rf = get_conn().db("ged").table("folder")
+        self.rt = get_conn().db("ged").table("timesheet")
+        self.ru = get_conn().db("ged").table("user")
+        
+    def get_all(self, page, number, search, client_type, email):
+        if page < 1:
+            page = 1
+        page -= 1
+        if number < 1:
+            number = 1
+        req = self.rt
+        if search is not None:
+            req = req.filter(
+                {
+                    "client": client_id
+                }
+            )
+        req = req.eqJoin("user_in_charge", self.ru).without({"right": {"id": True}}).zip().pluck(["id", "first_name", "last_name", "name", "type", "email"])
+        total = int(req.count().run())
+        max = math.floor(total / number + 1) if total % number != 0 else int(total/number)
+        max = max + 1 if max == 0 else max
+        if max < page + 1:
+            return [False, "Invalid pagination", 404]
+        pagination = {
+            "total": total,
+            "pages": {
+                "min": 1,
+                "max": max,
+                "per_page": number,
+                "actual_page": page + 1
+            }
+        }
+        return [True, {"list": list(req.run()), "pagination": pagination}, None]
+
 class TimesheetV2():
     def __init__(self):
         self.rf = get_conn().db("ged").table("folder")
