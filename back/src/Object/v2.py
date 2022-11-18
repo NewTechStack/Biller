8a281848cd63b6fcc57d7d05ec715fa04d7ece7d
SCcagg5 <sccagg5@gmail.com>
2022-11-18 11:00:58 +0100

    Update v2.py

--- back/src/Object/v2.py
+++ back/src/Object/v2.py
@@ -106,6 +106,7 @@ class TimesheetV2():
                {"status": status}
             )
         total = int(req.count().run())
+        req = req.order_by(index=r.desc('date'))
         req = req.eq_join(
             "client_folder", 
             self.rf
@@ -123,7 +124,7 @@ class TimesheetV2():
             {"right": "id"}
         ).zip().pluck(
             ["id", "date", "name", "desc", "user", "price", "status", "type", "duration", "image", "first_name", "last_name", "name_1", "name_2", "lang"]
-        ).order_by(index=r.desc('date')).skip(page * number).limit(number)
+        ).skip(page * number).limit(number)
         max = math.floor(total / number + 1) if total % number != 0 else int(total/number)
         max = max + 1 if max == 0 else max
         if max < page + 1:
@@ -192,6 +193,7 @@ class TimesheetV2():
             req = req.filter(
                {"status": status}
             )
+        req = req.order_by(index=r.desc('date'))
         total = int(
             req.eq_join("client_folder", self.rf).group("right").without("right").zip().ungroup().count().run()
         )
@@ -205,7 +207,7 @@ class TimesheetV2():
             ).eq_join(
                 "client_folder", 
                 self.rf
-            ).group("right").without("right").zip().ungroup().order_by(index=r.desc('date')).skip(page * number).limit(number).map(
+            ).group("right").without("right").zip().ungroup().skip(page * number).limit(number).map(
                 lambda doc:
                     {
                         "id": doc["group"]["id"], 
