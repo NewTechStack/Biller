95c7b145c506956e1d050039305be8547663a2c2
SCcagg5 <sccagg5@gmail.com>
2022-11-22 13:30:41 +0100

    Create bank.py

---
+++ back/src/Object/bank.py
@@ -0,0 +1,23 @@
+from .CRUD import Crud
+
+class Bank(Crud):
+    def __init__(self, id = None):
+        super().__init__(id, 'user')
+
+    def edit(self, internal_name, name, benef, iban, clearing, bic):
+        data = {
+            'id': self.id
+        }
+        if internal_name is not None:
+            data['internal_name'] = internal_name
+        if name is not None:
+            data['name'] = name
+        if benef is not None:
+            data['benef'] = benef
+        if iban is not None:
+            data['iban'] = iban
+        if clearing is not None:
+            data['clearing'] = clearing
+        if bic is not None:
+            data['bic'] = bic
+        return self._push(data)
