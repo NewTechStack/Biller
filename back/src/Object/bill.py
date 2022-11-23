5377c69bee6cc4187ffdba06b5a0bf1311e0c408
SCcagg5 <sccagg5@gmail.com>
2022-11-23 20:26:10 +0100

    Update bill.py

--- back/src/Object/bill.py
+++ back/src/Object/bill.py
@@ -369,5 +369,5 @@ class Bill(Crud, StatusObject):
 
         response = requests.request("POST", url, headers=headers, data=payload)
 
-        svg = json.loads(response.text)["svg"].replace("height='106mm'", "height='159mm'").replace("width='210mm'", "width='315mm'")
+        svg = json.loads(response.text)["svg"].replace("height='106mm'", "height='106mm'").replace("width='210mm'", "width='228mm'")
         return [True, svg, None]
