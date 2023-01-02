a845dca62fa9b4be76d6732e755737fc56559af3
SCcagg5 <sccagg5@gmail.com>
2023-01-02 10:19:48 +0100

    Update report.py

--- back/src/Object/report.py
+++ back/src/Object/report.py
@@ -1,1 +1,23 @@
-class Report().get()
+from .timesheet import Timesheet
+from .folder import Folder
+from .user import User
+from .bank import Bank
+import requests
+import uuid
+from datetime import datetime
+import json
+
+class Report():
+    def __init__(self, id = None):
+        pass
+    
+    def get(self):
+        url = "http://template:8080/template/pdf"
+        data = {
+            "name": f"report.html",
+            "title": f"damn",
+            "variables": {}
+        }
+        response = requests.request("POST", url, data=json.dumps(data), headers={'content-type': "application/json"})
+        return [True, json.loads(response.text), 200]
+      
