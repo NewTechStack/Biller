4da772666afefc021d2b176a05bebb4823780a83
SCcagg5 <sccagg5@gmail.com>
2023-01-03 01:54:27 +0100

    Update report.py

--- back/src/Object/report.py
+++ back/src/Object/report.py
@@ -11,6 +11,12 @@ class Report():
     def __init__(self, id = None):
         pass
     
+    def __currency_format(self, price):
+        return f"{price:_.2f}".replace(".00", ".-").replace("_", " ")
+    
+    def __hours_format(self, price):
+        return f"{price:_.2f}".split(".")[0] + "h"
+    
     def get(self):
         url = "http://template:8080/template/pdf"
         data = {
@@ -19,17 +25,27 @@ class Report():
             "bucket": "reports",
             "variables": {
                 "name": "Eliot Dujardin",
+                "total_hours": self.__hours_format(10),
+                "total_priced": self.__currency_format(1999) + " CHF",
+                "avg_price": self.__currency_format(300) + " CHF",
+                "total_payed": self.__currency_format(1599) + " CHF",
                 "lines": [
                     {
                         "name": "test",
-                        "payed": 80,
-                        "non_payed": 10,
-                        "billed": 10,
-                        "time": "10H30"
+                        "avg_price": self.__currency_format(300) + " CHF",
+                        
+                        "paid_percent": 80.
+                        "paid_price": self.__currency_format(1999) + " CHF",
+                        
+                        "billed_percent ": 10,
+                        "billed_price ": self.__currency_format(80) + " CHF",
+                        
+                        "non_percent": 10,
+                        "non_price": self.__currency_format(80) + " CHF",
+                        
+                        "time":  self.__hours_format(10)
                     }
                 ],
-                "total": "300.- CHF",
-                "total_payed": "30.- CHF"
             }
         }
         response = requests.request("POST", url, data=json.dumps(data), headers={'content-type': "application/json"})
