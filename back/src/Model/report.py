5dfcc7114435e1b1b4526608d8cad26286f055e5
SCcagg5 <sccagg5@gmail.com>
2023-01-05 03:42:53 +0100

    Update report.py

--- back/src/Model/report.py
+++ back/src/Model/report.py
@@ -2,5 +2,5 @@ from Controller.basic import check
 from Object.report import Report
 
 def report_get(cn, nextc):
-    err = Report().get()
+    err = Report().get(cn.pr["start"], cn.pr["end"], cn.pr["user_id"])
     return cn.call_next(nextc, err)
