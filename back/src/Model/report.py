0dcac8065ebff09a358143b04b33facfebbf57d9
SCcagg5 <sccagg5@gmail.com>
2023-01-05 03:45:36 +0100

    Update report.py

--- back/src/Model/report.py
+++ back/src/Model/report.py
@@ -2,5 +2,8 @@ from Controller.basic import check
 from Object.report import Report
 
 def report_get(cn, nextc):
+    err = check.contain(cn.pr, ["start", "end", "user_id"])
+    if not err[0]:
+        return cn.toret.add_error(err[1], err[2])
     err = Report().get(cn.pr["start"], cn.pr["end"], cn.pr["user_id"])
     return cn.call_next(nextc, err)
