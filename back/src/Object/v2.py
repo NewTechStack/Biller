d5143f062ca7bcf1af720a96249b3e43a696a216
SCcagg5 <sccagg5@gmail.com>
2023-01-10 15:34:54 +0100

    Update v2.py

--- back/src/Object/v2.py
+++ back/src/Object/v2.py
@@ -2,6 +2,7 @@ from .rethink import get_conn, r
 import urllib.parse
 import uuid
 import math
+import time
 
 class BillV2():
     def __init__(self):
@@ -225,17 +226,25 @@ class TimesheetV2():
                 lambda doc:
                     doc["date"] <= etime
             )
+       
+        ts = time.time()
+        print("start ", ts - time.time())
+        ts = time.time()
         total = int(req.count().run())
+        print("total ", ts - time.time())
+        ts = time.time()
         sum_arr = {
             "price": float(req.sum(lambda ts: ts["price"].mul(ts["duration"])).run()),
             "duration": float(req.sum('duration').run())
         }
+        print("sum ", ts - time.time())
+        ts = time.time()
         max = int(req.max(lambda timesheet: timesheet["order"][order])["order"][order].run())
-        print(max)
         req = req.filter(
         (r.row["order"][order] <= max - page * number) & (r.row["order"][order] >= max - (page + 1) * number)
         )
-        print(list(req.run()))
+        print("page ", ts - time.time())
+        ts = time.time()
         req = req.eq_join(
             "client_folder", 
             self.rf
@@ -254,6 +263,8 @@ class TimesheetV2():
         ).zip().pluck(
             ["id", "date", "name", "desc", "user", "price", "status", "type", "duration", "image", "first_name", "last_name", "name_1", "name_2", "lang"]
         ).order_by(r.desc("date"))
+        print("data ", ts - time.time())
+        ts = time.time()
         max = math.floor(total / number + 1) if total % number != 0 else int(total/number)
         max = max + 1 if max == 0 else max
         if max < page + 1:
@@ -267,7 +278,10 @@ class TimesheetV2():
                 "actual_page": page + 1
             }
         }
+        print("form ", ts - time.time())
+        ts = time.time()
         timesheets = list(req.run())
+        print("data2 ", ts - time.time())
         return [True, {"list": timesheets, "sum": sum_arr, "pagination": pagination}, None]
 
     def grouped_by_folder(self, page, number, client_id, folder_id, stime, etime, status, user):
