3c8d86fd23c737c915482818e7df8d96a67777ff
SCcagg5 <sccagg5@gmail.com>
2023-01-11 07:09:49 +0100

    Update timesheet.py

--- back/src/Object/timesheet.py
+++ back/src/Object/timesheet.py
@@ -55,3 +55,29 @@ class Timesheet(Crud, StatusObject):
             "order": {"user/client_folder": (r.row["order"]["user/client_folder"] + 1)}
         }).run()
         return self._push(input)
+    
+    def delete(self):
+        if self.id is None:
+            return [False, "Invalid id", 404]
+        res = dict(self.red.get(self.id).run())
+        order = res["order"]
+        self.red.filter({"client": res["client"]}).filter(r.row["order"]["client"] > res["order"]["client"]).update({
+            "order": {"client": (r.row["order"]["client"] - 1)}
+        }).run()
+        self.red.filter({"client_folder": res["client_folder"]}).filter(r.row["order"]["client_folder"] > res["order"]["client_folder"]).update({
+            "order": {"client_folder": (r.row["order"]["client_folder"] - 1)}
+        }).run()
+        self.red.filter({"id": res["id"]}).filter(r.row["order"]["id"] > res["order"]["id"]).update({
+            "order": {"id": (r.row["order"]["id"] - 1)}
+        }).run()
+        self.red.filter({"user": res["user"]}).filter(r.row["order"]["user"] > res["order"]["user"]).update({
+            "order": {"user": (r.row["order"]["user"] - 1)}
+        }).run()
+        self.red.filter({"user": res["user"], "client": res["client"]}).filter(r.row["order"]["user/client"] > res["order"]["user/client"]).update({
+            "order": {"user/client": (r.row["order"]["user/client"] - 1)}
+        }).run()
+        self.red.filter({"user": res["user"], "client_folder": res["client_folder"]}).filter(r.row["order"]["user/client_folder"] > res["order"]["user/client_folder"]).update({
+            "order": {"user/client_folder": (r.row["order"]["user/client_folder"] - 1)}
+        }).run()
+        ret = self.red.filter({'id': self.id}).delete().run()
+        return [True, {'id': self.id}, None]
