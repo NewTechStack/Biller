4be774689e0d3d516a0847af8364713799c94467
SCcagg5 <sccagg5@gmail.com>
2023-01-15 10:29:24 +0100

    Update script.py

--- back/script.py
+++ back/script.py
@@ -9,7 +9,7 @@ filters = [['client'], ['user'], ['client_folder'], ['user', 'client_folder'], [
 
 timesheets = list(time.order_by('date').pluck("id").run())
 for i in tqdm(range(len(timesheets))):
-    f = {"following": {"id": {"before": timesheets[i + 1]['id'] if i < len(timesheets + 1) else None, "after": timesheets[i - 1]['id'] if i > -1 else None}}}
+    f = {"following": {"id": {"before": timesheets[i + 1]['id'] if i < len(timesheets) + 1 else None, "after": timesheets[i - 1]['id'] if i > -1 else None}}}
     time.get(timesheets[i]['id']).update(f).run()
 
 for n in range(len(filters)):
@@ -19,5 +19,5 @@ for n in range(len(filters)):
         tri = tris[tri_i]
         timesheets = list(time.filter(tri).order_by('date').pluck("id").run())
         for i in range(len(timesheets)):
-            f = {"following": {'/'.join(filter): {"before": timesheets[i + 1]['id'] if i < len(timesheets + 1) else None, "after": timesheets[i - 1]['id'] if i > -1 else None}}}
+            f = {"following": {'/'.join(filter): {"before": timesheets[i + 1]['id'] if i < len(timesheets) + 1 else None, "after": timesheets[i - 1]['id'] if i > -1 else None}}}
             time.get(timesheets[i]['id']).update(f).run()
