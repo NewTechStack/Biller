122fe205231384dd17a12ce230f7d2317d91e94e
SCcagg5 <sccagg5@gmail.com>
2023-01-15 16:54:28 +0100

    Update timesheet.py

--- back/src/Object/timesheet.py
+++ back/src/Object/timesheet.py
@@ -43,8 +43,7 @@ class Timesheet(Crud, StatusObject):
     
     def insert_from_chain(self, date, actual_filter = {}, following = "id"):
         res = self.red.filter(actual_filter).filter(r.row["date"].ge("date")).min().default(None).run()
-        if res is not None: 
-            res = list(res)[0]
+        if res is not None:
             self.red.get(self.id).update({"following": {following: {"is_after_id": res["following"][following]["is_before_id"], "is_before_id": res["id"]}}}).run()
             if res["id"] is not None:
                 self.red.get(res["id"]).update({"following": {following: {"is_after_id": self.id}}}).run()
@@ -53,7 +52,6 @@ class Timesheet(Crud, StatusObject):
         else:
             res = self.red.filter(actual_filter).filter(r.row["date"].lt("date")).max().default(None).run()
             if res is not None:
-                res = list(res)[0]
                 self.red.get(self.id).update({"following": {following: {"is_after_id": res["id"], "is_before_id": None}}}).run()
             if res["following"][following]["is_after_id"] is not None:
                 self.red.get(res["following"][following]["is_after_id"]).update({"following": {following: {"is_before_id": self.id}}}).run()
