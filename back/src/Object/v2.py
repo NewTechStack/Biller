5fe47d9561de1d1e9892323cdf0343ebee4d9ce1
SCcagg5 <sccagg5@gmail.com>
2023-02-03 00:50:41 +0100

    Update v2.py

--- back/src/Object/v2.py
+++ back/src/Object/v2.py
@@ -339,7 +339,7 @@ class TimesheetV2():
         all_arr = dict(
             req.map(
                 lambda row: {"price" : row["price"].mul(row["duration"]), "duration" : row["duration"],  "total": 1}
-            ).reduce(
+            ).default({"price" : 0, "duration" : 0,  "total": 0}).reduce(
                 lambda left, right: {"price" : left["price"].add(right["price"].default(0)), "duration" : left["duration"].default(0).add(right["duration"].default(0)), "total": left["total"].add(right["total"].default(0))}
             ).default({"price" : 0, "duration" : 0,  "total": 0}).run()
         )
